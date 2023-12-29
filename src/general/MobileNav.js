0f2ce3fa0520237859df8893bbb749ffc822239a
SpiceWeasel66 <nagengasteva@gmail.com>
2023-12-29 13:59:42 +0000

    navbar links

---
+++ src/general/MobileNav.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MobileNav() {
+  return (
+    <div>
+      x
+    </div>
+  )
+}
+
+export default MobileNav
--- src/general/Navigation.js
+++ src/general/Navigation.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import React from 'react';
+import {NavLink} from 'react-router-dom';
+import Logo from '../media/spam.jpg';
+import MobileMenu from '../general/MobileNav';
 
-function Navigation() {
+function Navbar() {
   return (
-    <div>
-      
-    </div>
-  )
+    <nav>
+      <div>
+        <MobileMenu />
+      </div>
+      <NavLink to='/'>
+        <img
+          src={Logo}
+          alt='Logo'
+        />
+      </NavLink>
+      <ul>
+        <li>
+          <NavLink to='/'>Home</NavLink>
+        </li>
+        <li>
+          <NavLink to='about'>About</NavLink>
+        </li>
+        <li>
+          <NavLink to='contact'>Contact</NavLink>
+        </li>
+        <li>
+          <NavLink to='projects'>Projects</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 }
-
-export default Navigation
+export default Navbar;
