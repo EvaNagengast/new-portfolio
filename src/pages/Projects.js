0a1d5cafaabded49ec212d2f28be30b1b49f482f
SpiceWeasel66 <nagengasteva@gmail.com>
2023-12-30 23:45:26 +0000

    added project content

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,7 +1,85 @@
-import React from 'react'
+import React from 'react';
 
 function Projects() {
-  return <div>Hello from projects</div>;
+  return (
+    <div>
+      <p>
+        Personal Author Website OBJECTIVE: Build a beautiful Portfolio about
+        published work and recent releases ACHIEVED: I was lucky that Jane Corry
+        trusted me to build her a personal homepage. The aim was to design the
+        page based on her latest novel, the colour scheme was dark and gloomy
+        and to greet the visitor I constructed a flashing light in the windows
+        of the lakehouse, which proved harder than expected given the png
+        wouldn't load before the flashing windows at first, but I was very
+        proud, once it worked and gave the page a bit of a scary entry. Exactly
+        what we planned for. I added a slideshow to showcase her Books and an
+        extra section to present her reviews, which was important to her. During
+        this project I learned a lot about Routers and Naviation and dived deep
+        into the magic that can be created with React.
+      </p>
+      <p>
+        Musical Journey through London OBJECTIVE: Learn how to use embeds,
+        grids, and tables effectively. ACHIEVED: My favourite project so far,
+        this page was a joy to develop and a fun exploration of London's music
+        culture. I spent hours listening to songs named after London streets and
+        boroughs while designing and curating the content. I illustrated playful
+        elements like the "Way Out" button, and a moving TfL-Ticket after a 1991
+        travel card. It also features embeds, grids, and tables, all seamlessly
+        rendered responsive with the use of media queries.
+      </p>
+      <p>
+        Dictionary App OBJECTIVE: Deepen knowledge about React.js, looping,
+        mapping through objects, and utilising APIs and npm libraries. ACHIEVED:
+        This project helped me to expand my expertise in React.js, focusing on
+        looping and mapping through objects. It involved the creation of a
+        dictionary app, rich with features like images related to the search
+        term and an animated play buttons that talks to the user. The
+        development process was a fun and educational journey, helping me to
+        experiment with various npm libraries and to dive deeper into the
+        functionality of React.
+      </p>
+      <p>
+        Pigeon Showcase OBJECTIVE: I aimed to create a website that closely
+        resembled a Figma design, incorporating grids and ensuring
+        responsiveness. ACHIEVED: This project challenged me to dive into the
+        world of responsive design. I recreated a webpage based on a Figma
+        design, striving to stay as close to the original as possible while
+        personalising the content as a tribute to pigeons. Using a combination
+        of Bootstrap and media queries, I ensured that the site stays responsive
+        and beautiful across all devices.
+      </p>
+      <p>
+        Magic Weather App OBJECTIVE: Learn to use React.js and implement
+        multiple components ACHIEVED: As my first React project I built a second
+        Weather Application. Learning to use React was a steep learning curve.
+        Very quickly I enjoyed building all the components and putting them
+        together like a Puzzle. To add a unique touch and because autumn is
+        near, I created individual Tarot-themed weather icons.
+      </p>
+      <p>
+        Weather App OBJECTIVE: Build a functioning application, learn how to
+        implement APIs and make a responsive website using Bootstrap. ACHIEVED:
+        Building upon my previously-gained knowledge I constructed a Weather
+        App. This project helped me to learn how to integrate and understand
+        APIs, real-life development workflows, hosting, and to gain a deeper
+        understanding of JavaScript. The App allows users to easily access
+        up-to-date weather conditions and forecasts for locations all around the
+        world. I had lots of fun styling the website in a Pop Art style.
+      </p>
+      <p>
+        World Clock OBJECTIVE: Gain more experience with APIs, and especially
+        handling and processing local times. ACHIEVED: My second big project was
+        the development of a JavaScript World Clock App, also built using
+        Bootstrap. This project helped me to deepen my API knowledge and refine
+        my development skills. Implementing the time first seemed very hard, but
+        I loved reading about calculations around UTC, how to work with it and
+        to solve challenges creatively. I animated an indicator to show the
+        seconds and move around the main spplication like a clock.
+      </p>
+      <p></p>
+      <p></p>
+    </div>
+  );
 }
 
-export default Projects
+export default Projects;
