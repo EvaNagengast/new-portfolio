f58518724b90fe0ec9005cbc7236f5cd890b205d
SpiceWeasel66 <nagengasteva@gmail.com>
2023-12-30 23:52:37 +0000

    added links to portfolio

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -17,6 +17,13 @@ function Projects() {
         this project I learned a lot about Routers and Naviation and dived deep
         into the magic that can be created with React.
       </p>
+      <a
+        href='https://www.janecorryauthor.com/'
+        target='_blank'
+        rel='noreferrer'
+        alt='Jane Correy - author personal website'>
+        Visit page
+      </a>
       <p>
         Musical Journey through London OBJECTIVE: Learn how to use embeds,
         grids, and tables effectively. ACHIEVED: My favourite project so far,
@@ -27,6 +34,13 @@ function Projects() {
         travel card. It also features embeds, grids, and tables, all seamlessly
         rendered responsive with the use of media queries.
       </p>
+      <a
+        href='https://london-music-journey.netlify.app/'
+        target='_blank'
+        rel='noreferrer'
+        alt='Musical Journey through London Webpage'>
+        Visit page
+      </a>
       <p>
         Dictionary App OBJECTIVE: Deepen knowledge about React.js, looping,
         mapping through objects, and utilising APIs and npm libraries. ACHIEVED:
@@ -38,6 +52,13 @@ function Projects() {
         experiment with various npm libraries and to dive deeper into the
         functionality of React.
       </p>
+      <a
+        href='https://evas-dictionary.netlify.app/'
+        target='_blank'
+        rel='noreferrer'
+        alt='Dictionary Application'>
+        Visit page
+      </a>
       <p>
         Pigeon Showcase OBJECTIVE: I aimed to create a website that closely
         resembled a Figma design, incorporating grids and ensuring
@@ -48,6 +69,13 @@ function Projects() {
         of Bootstrap and media queries, I ensured that the site stays responsive
         and beautiful across all devices.
       </p>
+      <a
+        href='https://evas-pigeon-portfolio.netlify.app/'
+        target='_blank'
+        rel='noreferrer'
+        alt='Pigeon Showcase Webpage'>
+        Visit page
+      </a>
       <p>
         Magic Weather App OBJECTIVE: Learn to use React.js and implement
         multiple components ACHIEVED: As my first React project I built a second
@@ -56,6 +84,13 @@ function Projects() {
         together like a Puzzle. To add a unique touch and because autumn is
         near, I created individual Tarot-themed weather icons.
       </p>
+      <a
+        href='https://eva-weather-app-2.netlify.app/'
+        target='_blank'
+        rel='noreferrer'
+        alt='Magical Weather Application'>
+        Visit page
+      </a>
       <p>
         Weather App OBJECTIVE: Build a functioning application, learn how to
         implement APIs and make a responsive website using Bootstrap. ACHIEVED:
@@ -66,6 +101,13 @@ function Projects() {
         up-to-date weather conditions and forecasts for locations all around the
         world. I had lots of fun styling the website in a Pop Art style.
       </p>
+      <a
+        href='https://eva-weather-app-2.netlify.app/'
+        target='_blank'
+        rel='noreferrer'
+        alt='Magical Weather Application'>
+        Visit page
+      </a>
       <p>
         World Clock OBJECTIVE: Gain more experience with APIs, and especially
         handling and processing local times. ACHIEVED: My second big project was
@@ -76,8 +118,13 @@ function Projects() {
         to solve challenges creatively. I animated an indicator to show the
         seconds and move around the main spplication like a clock.
       </p>
-      <p></p>
-      <p></p>
+      <a
+        href='/'
+        target='_blank'
+        rel='noreferrer'
+        alt='Find out more about Eva Nagengast'>
+        Visit page
+      </a>
     </div>
   );
 }
