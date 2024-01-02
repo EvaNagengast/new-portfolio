745f3bb79dbf19444ae3af7d8d5237796eb89ec5
SpiceWeasel66 <nagengasteva@gmail.com>
2024-01-02 22:18:45 +0000

    css desktop menu

--- src/general/Navigation.js
+++ src/general/Navigation.js
@@ -2,23 +2,29 @@ import React from 'react';
 import {NavLink} from 'react-router-dom';
 import styles from './navigation.module.css';
 import Logo from '../media/spam.jpg';
-import MobileMenu from './MobileNav';
+import MobileMenu from './MobileMenu';
 import Menu from './Menu';
 
 function Navigation() {
   return (
     <nav className={styles.navigation}>
-      <div>
+      <div className={styles.mobileMenu}>
         <MobileMenu />
       </div>
-      <NavLink to='/'>
+      <NavLink
+        to='/'
+        className={({isActive}) => (isActive ? styles.noLogo : styles.logo)}>
         <img
           src={Logo}
           alt='Logo'
+          className={styles.logo}
         />
       </NavLink>
-      <Menu />
+      <div className={styles.menu}>
+        <Menu />
+      </div>
     </nav>
   );
 }
+
 export default Navigation;
