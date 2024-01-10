126d707884b9754e0cb953e029b76dcba69a2a78
SpiceWeasel66 <nagengasteva@gmail.com>
2024-01-10 16:07:25 +0000

    adjusted colours css

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,10 +4,11 @@ import Hero from '../Components/HomeComponents/Hero';
 import Intro from '../Components/HomeComponents/Intro';
 import Projects from '../Components/HomeComponents/Projects';
 import Contact from '../Components/HomeComponents/Contact';
+import styles from './home.module.css';
 
 function Home() {
   return (
-    <div>
+    <div className={styles.home}>
       <Hero />
       <Intro />
       <Projects />
