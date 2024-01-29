5f1536e2b58c454f1f770671832688894e0ff4fc
SpiceWeasel66 <nagengasteva@gmail.com>
2024-01-29 20:08:33 +0000

    footer css

--- src/general/Footer.js
+++ src/general/Footer.js
@@ -1,24 +1,25 @@
-import React from 'react'
+import React from 'react';
+import styles from './footer.module.css';
 
 function Footer() {
   return (
-    <div className='Footer'>
-      <footer className=''>
-        © 2023 Eva Nagengast. 
-        This page was crafted with care and built using React.js.
-      
-        It is
-        <a
-          href='https://github.com/EvaNagengast'
-          target='_blank'
-          rel='noreferrer'
-          alt='Github repository Eva Nagengast Portfolio'>
-          open-sourced
-        </a>
-        so feel free to have a look!
+    <div className={styles.footer}>
+      <footer>
+        <p>
+          This page was crafted with care and built using React.js. It is{' '}
+          <a
+            href='https://github.com/EvaNagengast'
+            target='_blank'
+            rel='noreferrer'
+            alt='Github repository Eva Nagengast Portfolio'>
+            open-sourced
+          </a>{' '}
+          so feel free to have a look!
+        </p>
+        <p>© 2023 Eva Nagengast.</p>
       </footer>
     </div>
   );
 }
 
-export default Footer
+export default Footer;
